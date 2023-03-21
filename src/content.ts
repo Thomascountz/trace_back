4542b6b99a69ecf75933f3981f91844d314fa9ad
Thomas Countz <thomascountz@gmail.com>
2023-03-21 23:30:23 +0100

    display Add annotation option

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,20 @@
-import "../dist/index.js";
+document.addEventListener("click", (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+  const details = target.closest(".details-reset");
+  if (details) {
+    const menu = details.querySelector(".dropdown-menu");
+    if (menu) {
+      const menuItem = menu?.querySelector("#js-add-annotation") as HTMLElement;
+      if (!menuItem) {
+        const menuItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = "#";
+        link.classList.add("dropdown-item");
+        link.id = "js-add-annotation";
+        link.textContent = "Add annotation";
+        menuItem.appendChild(link);
+        menu.appendChild(menuItem);
+      }
+    }
+  }
+});
